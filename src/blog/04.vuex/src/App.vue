82354e8e167c5fe05ab511f25b313d0463cc9a52
HvaiY <337646685@qq.com>
2020-12-16 13:28:54 +0800

    vuex初步使用

---
+++ src/blog/04.vuex/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app"> 
+    <h1>----------父计数-----------</h1>
+    {{this.$store.state.counter}}
+     
+   <input type="button" value="++" @click="increment">
+   <input type="button" value="--"  @click="decrement">
+     
+    <h1>--------子计数----------</h1>
+    <Counter :count="this.$store.state.counter"/>
+  </div>
+</template>
+
+<script>
+import Counter from './components/Counter.vue'
+import store from './store/index.js'
+export default {
+  name: 'App',
+  store,
+  components: {
+    Counter
+  },
+  data() {
+    return {
+     // count:1000
+    }
+  },
+  methods: {
+    decrement(){
+      this.$store.commit("decrement")
+    },
+    increment(){
+          this.$store.commit("increment")
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/blog/04.vuex/src/components/Counter.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>{{count}}</h2>
+    </div>
+</template>
+<script>
+export default {
+    name:"counter",
+    props:{
+        count:{
+            type:Number,
+            default:1000
+        }
+    }
+}
+</script>
+<style   scoped>
+
+</style>
